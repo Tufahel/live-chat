b0871697f7274ac829ac187bd63f140f6b7c3767
Tufahel Ahmed <60083437+Tufahel@users.noreply.github.com>
2021-10-26 02:42:41 +0600

    fist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <MessengerCustomerChat
+        pageId="106036471220216"
+        appId="1181132199027954"
+      />
     </div>
   );
 }
